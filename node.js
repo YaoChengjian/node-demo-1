7e30274e04436f9bd54f703a05fdbddd6d07ca60
yaochengjian <915248459@qq.com>
2020-02-20 15:33:14 +0800

    第六次部署

--- node.js
+++ node.js
@@ -22,7 +22,7 @@ var server = http.createServer(function(request, response) {
   /******** 从这里开始看，上面不要看 ************/
 
   console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
-
+  console.log(path);
   if (path === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
@@ -32,15 +32,15 @@ var server = http.createServer(function(request, response) {
     <head>
     <meta charset = "utf-8">
     <title>姚成健</title>
-      <link rel="stylesheet" href="./style">
+      <link rel="stylesheet" href="/style">
     </head>
     <body>
-      <h1>欢迎访问</h1>
+      <h1>欢迎大家来访问</h1>
     </body>
     </html>
     `);
     response.end();
-  } else if (path === "./style") {
+  } else if (path === "/style") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/css;charset=utf-8");
     response.write(`h1{color: red;}`);
